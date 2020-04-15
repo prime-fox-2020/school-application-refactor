9f1053551f2abd165ebecdcf7f6c9e1dda3d0981
unknown <sonymartha4@gmail.com>
2020-04-16 00:01:44 +0700

    edit didnt work properly

---
+++ routers/index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const router = express.Router()
+const DefaultController = require('../controllers/DefaultController')
+const TeacherController = require('../controllers/TeacherController')
+const SubjectController = require('../controllers/SubjectController')
+const StudentController = require('../controllers/StudentController')
+
+router.get('/', DefaultController.home)
+router.get('/teacher', TeacherController.get)
+router.get('/subject', SubjectController.get)
+router.get('/student', StudentController.get)
+router.get('/student/add', StudentController.addForm)
+router.post('/student/add', StudentController.add)
+router.get('/student/:id?/edit', StudentController.editForm)
+router.post('/student/:id?/edit', StudentController.edit)
+
+
+module.exports = router
